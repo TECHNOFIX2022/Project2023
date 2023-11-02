e251ae64973f879e1b4b73647abd08ad0d50d319
TECHNOFIX2022 <imrajkumar1030@gmail.com>
2023-11-02 19:47:27 +0530

    task completed

---
+++ src/org/hcl/Rajkumar.java
@@ -0,0 +1,16 @@
+package org.hcl;
+
+public class Rajkumar {
+public  void javaCourse() {
+System.out.println("Java course is Started");
+}	
+public  void PythonCourse() {
+	System.out.println("Python course is Started");
+}
+public static void main(String[] args) {
+	Rajkumar obj = new Rajkumar();
+	obj.javaCourse();
+	obj.PythonCourse();
+}
+
+}
